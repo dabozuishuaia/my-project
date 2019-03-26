23d3cb781b5aea407ddb830ae6dd074f6b4a7a0f
dabozuishuaia <550201393@qq.com>
2019-03-26 22:45:59 +0800

    新建模板

---
+++ src/components/usermes/usermes.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="usermes">
+        <h1 v-text="title"></h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'usermes',
+    props: ['title'],
+    data () {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
